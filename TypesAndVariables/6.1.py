4825894620aa59969acecbd928f86066444de9e5
s-A122-23 <s-A122-23@CAMPUS.UEK.KRAKOW.PL>
2025-10-25 12:56:51 +0200

    cd

--- TypesAndVariables/6.1.py
+++ TypesAndVariables/6.1.py
@@ -2,8 +2,8 @@
 # A program that calculates the number of characters
 # of your name, surname and full name
 #
-name = 'Anna'   # replace Anna with your name
-surname = 'May' # replace May with your surname
+name = 'Jan'   # replace Anna with your name
+surname = 'Sobczak' # replace May with your surname
 characters_in_name = len(name)
 print(f'Your name has {characters_in_name} characters')
 print(f'Your surname has {len(surname)} characters')
